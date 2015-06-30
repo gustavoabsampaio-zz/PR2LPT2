6bb7f1060946582072df0d03a11f5a3aa0b7ace5
Gustavo Sampaio <gustavoabsampaio@gmail.com>
2015-06-30 17:25:11 -0300

    funciona até a 7B

--- T2LP2.java
+++ T2LP2.java
@@ -4,37 +4,42 @@ import javax.swing.*;
 import java.util.Random;
 
 class T2LP2 implements ActionListener{
+
 	JFrame frame;
 	MyDrawPanel drawPanel;
+
 	public static void main(String[] args) {
 		T2LP2 prata = new T2LP2();
 		prata.go();
 	}
+
 	public void go(){
 		frame=new JFrame();
 		drawPanel=new MyDrawPanel();
-		
 		JPanel p1=new JPanel();
 		JButton b1 = new JButton("Gustavo");
 		JButton b2 = new JButton("Sampaio");
 		JButton b3 = new JButton("ERRAR!");
+
 		b1.addActionListener(this);
+		b2.addActionListener(new SobrenomeListener());
+
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(800,600);
 		frame.setVisible(true);
 		frame.getContentPane().add(p1,BorderLayout.EAST);
+		frame.getContentPane().add(drawPanel,BorderLayout.CENTER);
+
 		p1.add(b1);
 		p1.add(b2);
 		p1.add(b3);
-		frame.getContentPane().add(drawPanel,BorderLayout.CENTER);
-
 	}
+
 	public void actionPerformed(ActionEvent event)
 	{
 		frame.repaint();
-
-
 	}
+
 	class MyDrawPanel extends JPanel
 	{
 		public void paintComponent(Graphics g)
@@ -50,9 +55,16 @@ class T2LP2 implements ActionListener{
 			int gr= random.nextInt(200);
 			int b= random.nextInt(200);
 			Color cor = new Color(r,gr,b,60);
+
 			g.setColor(cor);
 			g.fillRoundRect(x,y,width,height,arcWidth,arcHeight);
 		}
 	}
 
+	class SobrenomeListener implements ActionListener
+	{
+		public void actionPerformed(ActionEvent event)
+		{
+		}
+	}
 }

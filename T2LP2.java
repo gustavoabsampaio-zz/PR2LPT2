83e3d71766558e137a0df15eed9c883dfc03beba
Gustavo Sampaio <gustavoabsampaio@gmail.com>
2015-06-30 16:12:41 -0300

    4B v2 - coloca tres botoes na tela

--- T2LP2.java
+++ T2LP2.java
@@ -9,14 +9,16 @@ class T2LP2 extends JPanel{
 	}
 	public void go(){
 		JFrame frame=new JFrame();
+		JPanel p1=new JPanel();
 		JButton b1 = new JButton("Gustavo");
 		JButton b2 = new JButton("Sampaio");
 		JButton b3 = new JButton("ERRAR!");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(800,600);
 		frame.setVisible(true);
-		frame.getContentPane().add(b1,BorderLayout.WEST);
-		frame.getContentPane().add(b2,BorderLayout.CENTER);
-		frame.getContentPane().add(b3,BorderLayout.EAST);
+		frame.getContentPane().add(p1,BorderLayout.EAST);
+		p1.add(b1);
+		p1.add(b2);
+		p1.add(b3);
 	}
 }
